8eb20e2ac6e9ca52bac437f3b9b141e1d681c889
jahedprince <jahedprince183@gmail.com>
2023-02-27 01:48:57 -0500

    added another 2 pointer problem

---
+++ #2 Two Pointers/3sum/3sum.js
@@ -0,0 +1,44 @@
+var threeSum = function (nums) {
+  // having the numbers in ascending order will make this problem much easier.
+  // also, knowing the overall problem  will take at least O(N^2) time, we can
+  // afford the O(NlogN) sort operation
+  nums.sort((a, b) => a - b);
+  const result = [];
+
+  //// `i` represents the "left" most number in our sorted set.
+  for (let i = 0; i < nums.length; i++) {
+    // `low` represents the "middle" element between `i` and `k`.
+    // we will increment this up through the array while `i` and `high`
+    // are anchored to their positions. we will decrement `high` for
+    // for each pass through the array, and finally increment `i`
+    // once `low` and `high meet.
+    let low = i + 1,
+      high = nums.length - 1,
+      sum = 0;
+
+    // to summarize our setup, we have `i` that starts at the beginning,
+    // `high` that starts at the end, and `low` that races in between the two.
+    //
+    // note that `i` is controlled by our outer for-loop and will move the slowest.
+    // in the meantime, `low` and `high` will take turns inching towards each other depending
+    // on some logic we'll set up below. once they collide, `i` will be incremented up
+    // and we'll repeat the process.
+
+    while (low < high) {
+      sum = nums[i] + nums[low] + nums[high];
+
+      if (sum === 0) {
+        result.push([nums[i], nums[low], nums[high]]);
+        while (nums[low + 1] === nums[low]) low++;
+        while (nums[high - 1] === nums[high]) high--;
+        low++;
+        high--;
+      } else if (sum < 0) low++;
+      else high--;
+    }
+    while (nums[i + 1] === nums[i]) i++;
+  }
+  return result;
+};
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
